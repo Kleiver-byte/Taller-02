0163d3c1f695dcf48c9eee6fa067f5f1642861ca
Kleiver-byte <kleiverpasaca15@gmail.com>
2024-10-17 20:33:48 -0500

    Este es mi segundo comentario

--- Proyecto01/src/proyecto01/Proyecto01.java
+++ Proyecto01/src/proyecto01/Proyecto01.java
@@ -6,7 +6,7 @@ public class Proyecto01 {
 
     
     public static void main(String[] args) {
-        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+        System.out.println("Manejado desde GitHub versionado a traves de Git creado por Kleiver-byte");
     }
     
 }
